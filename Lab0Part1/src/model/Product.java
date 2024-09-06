4f6b553e3eb61b0b2f827622e8b8030f6d49fb01
Abdulkadir Ahmed <sovxy@ea56.eecs.yorku.ca>
2024-09-06 18:06:54 -0400

    Code has been updated

--- Lab0Part1/src/model/Product.java
+++ Lab0Part1/src/model/Product.java
@@ -1,5 +1,23 @@
 package model;
 
-public class Product {
+/* 
+ * Products for apple refurbished store 
+ * BY Abdulkadir Ahmed
+ */
 
+public class Product {
+   /* Attributes */
+	private String model; // type of model
+	private String finish; // color of model
+	private int storage; // storage of model
+	private boolean hasCellularConnectivity; // false (wifi), true (wifi + data)
+	private double originalPrice; // Price of model e.g 1254.99
+	private double discountValue; // Discount of model e.g 250.00
+	
+	/* Constructors 
+	 * if no constructor are declared a implicit default constructor is there*/
+	
+	/* Accessors */
+	
+	/* Mutators */
 }
